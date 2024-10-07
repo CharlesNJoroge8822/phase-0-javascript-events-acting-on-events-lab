6b1464c1138c4b1c86e9511c783b958e440afab1
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-10-07 22:29:54 +0300

    changes

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Your code here
+function moveDodgerLeft (){
+    const dodger = document.getElementById ('dodger');
+    const leftNum = parseInt (dodger.style.left)
+    if (leftNum > 0) dodger.style.left = `${leftNum - 4}px`;
+
+}
+
+function moveDodgerRight(){
+    const dodger = document.getElementById('dodger');
+    const leftNum = parseInt(dodger.style.left);
+    if (leftNum < 360) dodger.style.left = `${leftNum + 4}px`
+
+}
+
+
